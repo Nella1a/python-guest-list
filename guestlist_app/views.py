adb2df3dda0e374725a660e6940a94bb4d63e118
Nella1a <m.allen@gmx.at>
2022-10-18 21:47:21 +0200

    Add ListView

--- guestlist_app/views.py
+++ guestlist_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import GuestList
 # Create your views here.
+
+class GuestListView(ListView):
+    model = GuestList
+    template_name = "guestlist_app/index.html"
