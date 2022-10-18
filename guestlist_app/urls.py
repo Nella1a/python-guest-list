07e7ffd6208f91c3355b9db20c9df337c0277c55
Nella1a <m.allen@gmx.at>
2022-10-18 22:13:38 +0200

    Add DeleteView

--- guestlist_app/urls.py
+++ guestlist_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import GuestListView
+from .views import DeleteGuest
 
 urlpatterns = [
-    path("", GuestListView.as_view(), name="index")
+    path("", GuestListView.as_view(), name="index"),
+    path("guest/<int:pk>/", DeleteGuest.as_view(), name="del_guest")
 ]
--- guestlist_app/views.py
+++ guestlist_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DeleteView
 from .models import GuestList
+from django.urls import reverse
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 class GuestListView(ListView):
     model = GuestList
     template_name = "guestlist_app/index.html"
+
+class DeleteGuest(DeleteView):
+    model = GuestList
+    success_url = reverse_lazy("index")
