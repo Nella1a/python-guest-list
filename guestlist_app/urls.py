4c760b26508177f9ed3199e18638ac9a01fafb4d
Nella1a <m.allen@gmx.at>
2022-10-18 22:46:51 +0200

    Refactor DeleteView, Add CreateView

--- guestlist_app/urls.py
+++ guestlist_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import GuestListView
 from .views import DeleteGuest
+from .views import CreateGuest
 
 urlpatterns = [
     path("", GuestListView.as_view(), name="index"),
-    path("guest/<int:pk>/", DeleteGuest.as_view(), name="del_guest")
+    path("guest/<int:pk>/delete", DeleteGuest.as_view(), name="del_guest"),
+    path("guest/add/", CreateGuest.as_view(), name="add_guest"),
 ]
--- guestlist_app/views.py
+++ guestlist_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DeleteView
+from django.views.generic import CreateView
 from .models import GuestList
 from django.urls import reverse
 from django.urls import reverse_lazy
@@ -14,3 +15,19 @@ class GuestListView(ListView):
 class DeleteGuest(DeleteView):
     model = GuestList
     success_url = reverse_lazy("index")
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["delete_all"] = self.kwargs["del_all"]
+        return context
+
+class CreateGuest(CreateView):
+    model = GuestList
+    fields = [
+        "first_name",
+        "last_name"
+    ]
+
+    def get_success_url(self):
+        return reverse_lazy("index")
+
