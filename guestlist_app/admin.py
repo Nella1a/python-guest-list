deddbb1312ab7aad32de0f861f672ac3cf9ee252
Nella1a <m.allen@gmx.at>
2022-10-21 21:25:02 +0200

    Refactor models

--- guestlist_app/admin.py
+++ guestlist_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import GuestList
+from .models import Guests
 
 # Register your models here.
 
 admin.site.register(GuestList)
+admin.site.register(Guests)
--- guestlist_app/models.py
+++ guestlist_app/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 # Create your models here.
-
 class GuestList(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name_plural = "Guestlists"
+
+
+
+class Guests(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     attending = models.BooleanField(default=True)
+    list = models.ForeignKey(GuestList, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
 
+    class Meta:
+        verbose_name_plural = "Guests"
